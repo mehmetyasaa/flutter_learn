21b44c60aea0835c984b597b158295d8099ae235
Mehmet Yasa <mehmetyasaa@outlook.com>
2024-02-11 14:09:00 +0300

    firebase_view.dart add

---
+++ lib/views/firebase_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FirebaseBookView extends StatefulWidget {
+  const FirebaseBookView({super.key});
+
+  @override
+  State<FirebaseBookView> createState() => _FirebaseBookViewState();
+}
+
+class _FirebaseBookViewState extends State<FirebaseBookView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
